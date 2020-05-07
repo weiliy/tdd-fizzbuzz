1145efb8adb698dd548bfcfac6e583cb47e99e87
Weili Yi <wlyi@thoughtworks.com>
2020-05-07 17:12:43 +0800

    feat: only multiplication of three shold return fizz

--- src/main/java/cn/xpbootcamp/fizzbuzz/FizzBuzz.java
+++ src/main/java/cn/xpbootcamp/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,9 @@ package cn.xpbootcamp.fizzbuzz;
 
 public class FizzBuzz {
     public String report(int n) {
+        if (n % 3 == 0) {
+            return "Fizz";
+        }
         return String.valueOf(n);
     }
 }
